9c2300c8aca36fe18dccce146ee42045c099ecc5
pedrosimiao <pedrolumiere@gmail.com>
2024-03-19 18:34:14 -0300

    componentizando a aplicação

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const props = defineProps(['tarefas'])
+</script>
+<template>
+    <!-- Desafio: renderizar tarefas pendentes somente se existirem tarefas pendentes (usar o v-if para isso) -->
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.finalizada" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
